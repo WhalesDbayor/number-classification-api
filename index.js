f339127fc19d43652dfa30771b975a67a648c67c
Whales D'bayor <wabalonline01@gmail.com>
2025-02-04 02:36:50 +0100

    Fixed index.js

---
+++ index.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Enable CORS
+app.use(cors());
+
+// Function to check if a number is prime
+const isPrime = (num) => {
+    if (num < 2) return false;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) return false;
+    }
+    return true;
+};
+
+// Function to check if a number is perfect
+const isPerfect = (num) => {
+    let sum = 1;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) {
+            sum += i;
+            if (i !== num / i) sum += num / i;
+        }
+    }
+    return sum === num && num !== 1;
+};
+
+// Function to check if a number is an Armstrong number
+const isArmstrong = (num) => {
+    const digits = num.toString().split(""),
+          length = digits.length;
+    const sum = digits.reduce((acc, digit) => acc + Math.pow(parseInt(digit), length), 0);
+    return sum === num;
+};
+
+// Function to get a fun fact from Numbers API
+const getFunFact = async (num) => {
+    try {
+        const response = await axios.get(`http://numbersapi.com/${num}/math`);
+        return response.data;
+    } catch (error) {
+        return "No fun fact available.";
+    }
+};
+
+// Number classification API endpoint
+app.get("/api/classify-number", async (req, res) => {
+    const { number } = req.query;
+    const num = parseInt(number);
+
+    if (isNaN(num)) {
+        return res.status(400).json({ number, error: true });
+    }
+
+    const properties = [];
+    if (isArmstrong(num)) properties.push("armstrong");
+    properties.push(num % 2 === 0 ? "even" : "odd");
+
+    const funFact = await getFunFact(num);
+
+    res.json({
+        number: num,
+        is_prime: isPrime(num),
+        is_perfect: isPerfect(num),
+        properties,
+        digit_sum: num.toString().split("").reduce((sum, digit) => sum + parseInt(digit), 0),
+        fun_fact: funFact,
+    });
+});
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
